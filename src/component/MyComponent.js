36b94d3bb42f99be7cb211d4d90d67bcc35e37f1
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-06-18 23:05:17 +0700

    update_state

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -5,13 +5,24 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Cuong',
+        address: 'Ha Noi',
+        age: 18
+    };
+
+
     //tập hơp những khối HTML chuyển thành giao diện
     //Cú pháp JSX (JavaScript XML) là một phần của React, nó cho phép chúng ta viết HTML trong JavaScript
     render() {
-        return <div>MyComponent
-
-            {Math.random() * 1000}
-        </div>;
+        return (
+            <div>
+                <h1>My name is {this.state.name}</h1>
+                <h2>I am {this.state.age} years old</h2>
+                <h3>I am living in {this.state.address}</h3>
+            </div>
+        );
     }
 }
 //SD class
