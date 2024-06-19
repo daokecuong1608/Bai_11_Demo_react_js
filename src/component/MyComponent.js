6d680ca7ff3c6ce8028bdddab5aa600b98a8c055
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-06-19 14:53:41 +0700

    sử lý sự kiện react

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -12,6 +12,13 @@ class MyComponent extends React.Component {
         age: 18
     };
 
+    handleClick = (event) => {
+        alert('Hello');
+        console.log(event.target);
+    }
+    handleOnMoveOver(event) {
+        console.log(event.target);
+    }
 
     //tập hơp những khối HTML chuyển thành giao diện
     //Cú pháp JSX (JavaScript XML) là một phần của React, nó cho phép chúng ta viết HTML trong JavaScript
@@ -21,6 +28,8 @@ class MyComponent extends React.Component {
                 <h1>My name is {this.state.name}</h1>
                 <h2>I am {this.state.age} years old</h2>
                 <h3>I am living in {this.state.address}</h3>
+                <button onMouseOver={this.handleOnMoveOver}>Click me</button>
+
             </div>
         );
     }
