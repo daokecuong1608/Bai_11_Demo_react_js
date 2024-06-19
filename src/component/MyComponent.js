486b1ce392d3730b00ca17c992dcd31c741837e4
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-06-19 15:10:42 +0700

    update state

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -15,6 +15,13 @@ class MyComponent extends React.Component {
     handleClick = (event) => {
         alert('Hello');
         console.log(event.target);
+        this.setState({
+            name: 'AAAA'
+        })
+
+        this.setState({
+            age: Math.floor((Math.random() * 100) + 1)
+        })
     }
     handleOnMoveOver(event) {
         console.log(event.target);
@@ -29,6 +36,7 @@ class MyComponent extends React.Component {
                 <h2>I am {this.state.age} years old</h2>
                 <h3>I am living in {this.state.address}</h3>
                 <button onMouseOver={this.handleOnMoveOver}>Click me</button>
+                <button onClick={this.handleClick}>Click me</button>
 
             </div>
         );
