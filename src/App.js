7704e93bf165d27b500d4d7687761b07d172864c
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-06-26 15:15:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.scss';
 import Header from './component/header/Header';
+import { Outlet, Link } from "react-router-dom";
 
 
 const App = () => {
@@ -8,7 +9,17 @@ const App = () => {
   return (
     <div className="app-container">
       <Header />
-
+      <div>
+        test link
+      </div>
+      <div>
+        <button>
+          <Link to="/users">go to User</Link>
+        </button>
+        <button>
+          <Link to="/admins">go to Admin</Link>
+        </button>
+      </div>
     </div>
 
   );
